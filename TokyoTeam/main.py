6d5723c5881eb4a1acd8fc07a33de69b88047f30
emenesesayc <30834816+emenesesayc@users.noreply.github.com>
2017-11-29 23:23:08 -0300

    Update main.py

--- TokyoTeam/main.py
+++ TokyoTeam/main.py
@@ -3,55 +3,83 @@ from pygame.locals import*
 import random
 
 from scoreboards import *
+from menu import *
+from game import *
 
 pygame.init()
 pygame.mouse.set_visible(False)
 
 
-
 def main():
+	class Error(Exception):
+		pass
+	class NotFound(Error):
+		pass
+
 	scene = "menu"
 	scores = getHighScores()
 
-	screen = pygame.display.set_mode((450,500))
+	joysticks = []
+	for i in range(pygame.joystick.get_count()):
+		joysticks.append(pygame.joystick.Joystick(i))
+		joysticks[-1].init()
+	joystick_control = False
+
+	#screen = pygame.display.set_mode((1300,700))
+	resolution = pygame.display.Info()
+	screen = pygame.display.set_mode((resolution.current_w, resolution.current_h), pygame.FULLSCREEN)
+
+
+	#intro(screen)
+
+
 	
-	string = keyboard(screen)
-	
-	playing = True
-	while playing:
-		screen.fill((0,0,0))
+	while scene != "Exit" :
 
-		for evento in pygame.event.get():
-
-			if evento.type == QUIT:
-				playing = False
-
-
-			elif evento.type == pygame.KEYDOWN:
-				if evento.key == pygame.K_ESCAPE:
-					playing = False
-
+		try:		
+			if scene == "Exit":
+				pass
+			elif scene == "menu":
+				scene = menu(screen, joysticks, joystick_control)
+			elif scene == "High Scores":
+				scene = scoreboards(screen, scores, joysticks, joystick_control)
+			elif scene == "Arcade":
+				score = arcade(screen, joysticks, joystick_control)
+				if score > scores[-1][0]:
+					name = keyboard(screen, joysticks, joystick_control)
+					scores[-1] = [score, name]
+					scores.sort(reverse=True)
+					screen.fill((0,0,0))
+				scene = restart(screen, joysticks, joystick_control, score)
+			else:
+				raise NotFound
+		except NotFound:
+			scene = Error404(screen, joysticks)
 		
-
-		"""
-		if scene == "menu":
-			scene = menu()
-		elif scene == "scoreboards":
-			scene = scoreboards()
-		elif scene == "storymode":
-			pass
-		elif scene == "arcade":
-			score = game()
-			if score > min(scores):
-				name = keyboard()
-				score.index(min(scores)) = [score, name]
-			scene = restart()
+		
 			
-
-		"""
-
-		pygame.display.update()
-
+	saveHighScores(scores)
 	pygame.display.quit()
 
+def Error404(screen, joysticks):
+	screen.fill((255,255,255))
+	fuente5 = pygame.font.Font(None, 80)
+	msg = fuente5.render("Error 404", 1, (0,0,0))
+	screen.blit(msg, (300,300))
+
+
+	pygame.display.update()
+
+	while True:
+		for evento in pygame.event.get():
+			if evento.type == pygame.JOYBUTTONDOWN:
+				return "menu"
+			elif evento.type == pygame.JOYAXISMOTION:
+				return "menu"
+			elif evento.type == pygame.KEYDOWN:
+				return "menu"
+
+
+
 main()
+
